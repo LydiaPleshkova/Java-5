79e3835d3e296e6297a1007c7149a7a91ae0798e
Lydia Pleshkova <lydia.y.pleshkova@gmail.com>
2023-04-17 22:05:22 +0300

    test changed

--- src/test/java/ru/netology/services/WorkLifeBalanceServiceTest.java
+++ src/test/java/ru/netology/services/WorkLifeBalanceServiceTest.java
@@ -1,6 +1,8 @@
+package ru.netology.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.netology.services.WorkLifeBalanceService;
+//import ru.netology.services.WorkLifeBalanceService;
 
 public class WorkLifeBalanceServiceTest {
 
@@ -9,13 +11,10 @@ public class WorkLifeBalanceServiceTest {
         WorkLifeBalanceService service = new WorkLifeBalanceService();
 
         // подготавливаем данные:
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
         int expected = 3;
 
         // вызываем целевой метод:
-        int actual = service.calculate(income, expenses, threshold);
+        int actual = service.calculate(10_000, 3_000, 20_000);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
@@ -26,13 +25,10 @@ public class WorkLifeBalanceServiceTest {
         WorkLifeBalanceService service = new WorkLifeBalanceService();
 
         // подготавливаем данные:
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
         int expected = 2;
 
         // вызываем целевой метод:
-        int actual = service.calculate(income, expenses, threshold);
+        int actual = service.calculate(100_000, 60_000, 150_000);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
