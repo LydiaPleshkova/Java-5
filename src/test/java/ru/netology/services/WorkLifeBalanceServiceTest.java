dda11569faddfea49c164491269bc8cc2c828bcf
Lydia Pleshkova <lydia.y.pleshkova@gmail.com>
2023-04-17 22:34:27 +0300

    parametrized test added

--- src/test/java/ru/netology/services/WorkLifeBalanceServiceTest.java
+++ src/test/java/ru/netology/services/WorkLifeBalanceServiceTest.java
@@ -2,35 +2,43 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 //import ru.netology.services.WorkLifeBalanceService;
 
 public class WorkLifeBalanceServiceTest {
 
-    @Test
-    void shouldCalculateForHighThreshold() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "3,10000,3000,20000",
+//            "2,100000,60000,150000"
+//    })
+    @CsvFileSource(files="src/test/resources/WorkLifeBalance.csv")
+    void shouldCalculateForHighThreshold(int expected, int income, int expenses, int threshold) {
         WorkLifeBalanceService service = new WorkLifeBalanceService();
 
         // подготавливаем данные:
-        int expected = 3;
+        //int expected = 3;
 
         // вызываем целевой метод:
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    void shouldCalculateForLowThreshold() {
-        WorkLifeBalanceService service = new WorkLifeBalanceService();
+        //   }
+ //   @Test
+ //   void shouldCalculateForLowThreshold() {
+ //       WorkLifeBalanceService service = new WorkLifeBalanceService();
 
         // подготавливаем данные:
-        int expected = 2;
+ //       int expected = 3;
 
         // вызываем целевой метод:
-        int actual = service.calculate(100_000, 60_000, 150_000);
+ //       int actual = service.calculate(100_000, 60_000, 150_000);
 
         // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
+//        Assertions.assertEquals(expected, actual);
+//   }
 }
